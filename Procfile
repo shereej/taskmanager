073932779249f8a76a44b7d211230580a392504d
shereej <pshereej@gmail.com>
2018-04-15 18:04:09 +0530

    Updating Files

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: react-scripts start
-api: nodemon server.js --exec babel-node --presets es2015,stage-2
+api: nodemon server.js
--- server.js
+++ server.js
@@ -1,36 +1,114 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
-import { makeExecutableSchema } from 'graphql-tools';
-import mongoose from 'mongoose';
+//server.js
+'use strict'
+//first we import our dependencies…
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var Comment = require('./model/comments');
 
-var typeDefs = [`
-type Query {
-  hello: String
-}
+//and create our instances
+var app = express();
+var router = express.Router();
+//set our port to either a predetermined port number if you have set 
+//it up, or 3001
+var port = process.env.API_PORT || 3001;
 
-schema {
-  query: Query
-}`];
+//mongo db connect 
+const dbURI =  'mongodb://localhost/todo';
+mongoose.connect(dbURI);
 
-var resolvers = {
-  Query: {
-    hello(root) {
-      return 'world';
-    }
-  }
-};
-
-
-var schema = makeExecutableSchema({
-    typeDefs,
-	  resolvers
+mongoose.connection.once('connected', () => {
+    console.log(`MongoDB connection connection opened to: ${dbURI} - ${new Date()}`);
 });
 
-var app = express();
+mongoose.connection.on('error', (err) => {
+    console.log(`MongoDB connection error: ${err}`);
+});
 
-app.use('/graphql', cors(), bodyParser.json(), graphqlExpress({ schema }));
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql'}));
+mongoose.connection.once('disconnected', () => {
+    console.log('MongoDB disconnected');
+});
 
-app.listen(4000, () => console.log('Now app is running on 4000!') );
+//now we should configure the API to use bodyParser and look for 
+//JSON data in the request body
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//To prevent errors from Cross Origin Resource Sharing, we will set 
+//our headers to allow CORS with middleware like so:
+//To prevent errors from Cross Origin Resource Sharing, we will set 
+//our headers to allow CORS with middleware like so:
+app.use(function(req, res, next) {
+ res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Credentials', 'true');
+ res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+ res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+//and remove cacheing so we get the most recent comments
+ res.setHeader('Cache-Control', 'no-cache');
+ next();
+});
+//now we can set the route path & initialize the API
+router.get('/', function(req, res) {
+ res.json({ message: 'API Initialized!'});
+});
+
+//adding the /comments route to our /api router
+router.route('/comments')
+ //retrieve all comments from the database
+    .get(function(req, res) {
+        //looks at our Comment Schema
+        Comment.find(function(err, comments) {
+            if (err)
+            res.send(err);
+            //responds with a json object of our database comments.
+            res.json(comments)
+        });
+    })
+    //post new comment to the database
+    .post(function(req, res) {
+    var comment = new Comment();
+    //body parser lets us use the req.body
+    comment.name = req.body.name;
+    comment.tasklist = req.body.tasklist;
+    comment.save(function(err) {
+        if (err)
+        res.send(err);
+        res.json({ message: 'Comment successfully added!' });
+    });
+});
+
+
+//Adding a route to a specific comment based on the database ID
+router.route('/comments/:comment_id')
+ .put(function(req, res) {
+	Comment.findById(req.params.comment_id, function(err, comment) {
+	 if (err)
+	 res.send(err);
+	//setting the new author and text to whatever was changed. If 
+	//nothing was changed we will not alter the field.
+	
+	(req.body.tasklist) ? comment.tasklist = req.body.tasklist : null;
+		//save comment
+		comment.save(function(err) {
+		 if (err)
+		 res.send(err);
+			res.json({ message: 'Comment has been updated' });
+		});
+	});
+ })
+ //delete method for removing a comment from our database
+ .delete(function(req, res) {
+	 //selects the comment by its ID, then removes it.
+	 Comment.remove({ _id: req.params.comment_id }, function(err, comment) {
+		 if (err)
+		 res.send(err);
+		 res.json({ message: 'Comment has been deleted' })
+	 })
+ });
+
+//Use our router configuration when we call /api
+app.use('/todo', router);
+//starts the server and listens for requests
+app.listen(port, function() {
+    console.log(`api running on port ${port}`);
+});
